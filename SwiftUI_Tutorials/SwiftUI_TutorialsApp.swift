ffc4268a7be91f6f9b92f90a4fcad2ac1ef0fc31
Nguyen Le Tuan <mynillstar@gmail.com>
2022-10-08 19:56:24 +0200

    Initial Commit

---
+++ SwiftUI_Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Tuấn Nguyễn on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
